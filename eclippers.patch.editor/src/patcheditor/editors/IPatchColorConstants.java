0030ab7c4552c911e741668e87e9a0426723a263
Jennifer Baldwin <jebaldwin@gmail.com>
2012-03-10 01:43:58 -0800

    new patch editor style

--- eclippers.patch.editor/src/patcheditor/editors/IPatchColorConstants.java
+++ eclippers.patch.editor/src/patcheditor/editors/IPatchColorConstants.java
@@ -8,4 +8,8 @@ public interface IPatchColorConstants {
 	RGB MINUS = new RGB(102, 51, 255);
 	RGB ADD = new RGB(51, 153, 255);
 	RGB DIFF = new RGB(51, 125, 51);
+	RGB WHITE = new RGB(255, 255, 255);
+	RGB GREEN = new RGB(193, 255, 193);
+	RGB RED = new RGB(255,182,193);
+	RGB BLUE = new RGB(50,50,255);
 }
